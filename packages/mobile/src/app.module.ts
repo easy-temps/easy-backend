a4834fa6a50035d6236e108535b0fdccee127f65
CharleeWa <18888351756@163.com>
2024-05-15 15:43:02 +0800

    chore: format code using Prettier

--- packages/mobile/src/app.module.ts
+++ packages/mobile/src/app.module.ts
@@ -3,14 +3,16 @@ import { AppController } from './controller/app/app.controller';
 import { AppService } from './service/app.service';
 import { ConfigModule } from '@nestjs/config';
 
-import configuration from './config/configuration'
-import {ProseController} from "./controller/project/prose/proseController";
+import configuration from './config/configuration';
+import { ProseController } from './controller/project/prose/proseController';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    load:[configuration]
-  })],
-  controllers: [AppController,ProseController],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+  ],
+  controllers: [AppController, ProseController],
   providers: [AppService],
 })
 export class AppModule {}
