cb925a75c4ec3d1853036db7c9c8037c1d0485a3
CharleeWa <18888351756@163.com>
2024-05-16 17:48:48 +0800

    feat: enable cors

--- packages/mobile/src/main.ts
+++ packages/mobile/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.enableCors();
   await app.listen(3012);
 }
 bootstrap();
