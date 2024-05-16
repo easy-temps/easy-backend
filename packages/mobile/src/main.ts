0ff807f6c0126933987c3f148af21107f430788c
CharleeWa <18888351756@163.com>
2024-05-16 17:20:49 +0800

    feat: add `/api` global prefix

--- packages/mobile/src/main.ts
+++ packages/mobile/src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(3012);
 }
 bootstrap();
